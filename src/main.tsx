f504b8afb411a19b51bb37be27f80058c1ea86b9
BobbyKJH <rlatnguscjsw@gmail.com>
2023-02-04 00:01:49 +0900

    Feat: main.tsx
    1. 내용
    
    - react-query 연결
    
    2. Git Type
    
    1) Feat: 새로운 기능 개발
    
    2) Fix: 버그 등 수정
    
    3) Docs: 문서 내용 변경
    
    4) Style: 코드가 아닌 스타일에 관련된 수정
    
    5) Refactor: 리팩토링 코드

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,19 @@
 import ReactDOM from "react-dom/client";
 
+import { BrowserRouter } from "react-router-dom";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 import App from "./App";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  </BrowserRouter>
 );
