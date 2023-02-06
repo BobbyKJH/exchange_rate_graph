d22cb6ea680f453571691c80a67373ca0420e018
BobbyKJH <rlatnguscjsw@gmail.com>
2023-02-07 00:05:26 +0900

    Refactor: App.tsx Route
    1. 내용
    
    - Route Calculator Page delete
    
    2. Git Type
    
    1) Feat: 새로운 기능 개발
    
    2) Fix: 버그 등 수정
    
    3) Docs: 문서 내용 변경
    
    4) Style: 코드가 아닌 스타일에 관련된 수정
    
    5) Refactor: 리팩토링 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-const App = () => {
-  return <></>;
-};
+import { Route, Routes } from "react-router-dom";
 
+import GraphPage from "./page/GraphPage";
+import HomePage from "./page/HomePage";
+
+import Header from "./components/common/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/graph" element={<GraphPage />} />
+      </Routes>
+    </>
+  );
+};
 export default App;
