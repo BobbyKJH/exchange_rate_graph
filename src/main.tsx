eb98e470061fa8f0cf3bb1b4d8efce20286b6ac9
BobbyKJH <rlatnguscjsw@gmail.com>
2023-02-07 00:06:30 +0900

    Feat: main.tsx Redux Provider
    1. 내용
    
    - Redux Provider connect
    
    2. Git Type
    
    1) Feat: 새로운 기능 개발
    
    2) Fix: 버그 등 수정
    
    3) Docs: 문서 내용 변경
    
    4) Style: 코드가 아닌 스타일에 관련된 수정
    
    5) Refactor: 리팩토링 코드

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,20 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
 import App from "./App";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={true} />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
+    </Provider>
   </BrowserRouter>
 );
