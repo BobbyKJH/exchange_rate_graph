dc493981ef929ee0c4fe2d3f0ed5f077964c6ec2
BobbyKJH <rlatnguscjsw@gmail.com>
2023-02-16 00:22:23 +0900

    Feat: Redux persist && Reset style
    1. 내용
    
    - redux presist connect
    - reset css connect
    
    2. Git Type
    
    1) Feat: 새로운 기능 개발
    
    2) Fix: 버그 등 수정
    
    3) Docs: 문서 내용 변경
    
    4) Style: 코드가 아닌 스타일에 관련된 수정
    
    5) Refactor: 리팩토링 코드

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,28 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { persistStore } from "redux-persist";
 import { store } from "./store/store";
 
 import App from "./App";
 
+import { ResetStyle } from "./style/common/Reset.styled";
+
 const queryClient = new QueryClient();
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
+        <PersistGate loading={null} persistor={persistor} />
+
+        <ResetStyle />
+
         <App />
+
         <ReactQueryDevtools initialIsOpen={true} />
       </QueryClientProvider>
     </Provider>
