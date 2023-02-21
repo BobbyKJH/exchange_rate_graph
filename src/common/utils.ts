f69969ce0648caa71f09716a70421592fb9727ed
BobbyKJH <rlatnguscjsw@gmail.com>
2023-02-22 00:00:32 +0900

    Refactor: pathUrl return type 설정
    1. 내용
    
    - pathUrl return type boolean 값 설정
    
    2. Git Type
    
    1) Feat: 새로운 기능 개발
    
    2) Fix: 버그 등 수정
    
    3) Docs: 문서 내용 변경
    
    4) Style: 코드가 아닌 스타일에 관련된 수정
    
    5) Refactor: 리팩토링 코드

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
 
-export const pathUrl = (url: string) => {
+export const pathUrl = (url: string): boolean => {
   const { pathname } = useLocation();
 
   return pathname.includes(`/${url}`) ? true : false;
