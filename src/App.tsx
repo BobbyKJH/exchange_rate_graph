a858b6d8250e04ef3f2d6de8db5377b7488c8f14
BobbyKJH <rlatnguscjsw@gmail.com>
2023-02-25 15:03:50 +0900

    Feat: Calculator Page connect
    1) 내용
    
    - 계산기 페이지 route 연결
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+/* React-Router-Dom */
 import { Route, Routes } from "react-router-dom";
-
+/* Page */
 import GraphPage from "./page/GraphPage";
 import HomePage from "./page/HomePage";
-
+import ListPage from "./page/ListPage";
+import CalculatorPage from "./page/CalculatorPage";
+/* Component */
 import Header from "./components/common/Header";
 
 const App = () => {
@@ -13,6 +16,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/graph" element={<GraphPage />} />
+        <Route path="/graph/list" element={<ListPage />} />
+        <Route path="/calculator" element={<CalculatorPage />} />
       </Routes>
     </>
   );
