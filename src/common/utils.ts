fba738bc549c8bf7a2a0bedbcb8c905f00d5a611
BobbyKJH <rlatnguscjsw@gmail.com>
2023-03-08 21:48:14 +0900

    Refactor: React -> Next 변경
    1) 내용
    
    - Next.js 변경 예정
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- src/common/utils.ts
+++
@@ -1,7 +0,0 @@
-import { useLocation } from "react-router-dom";
-
-export const pathUrl = (url: string): boolean => {
-  const { pathname } = useLocation();
-
-  return pathname.includes(`/${url}`);
-};
