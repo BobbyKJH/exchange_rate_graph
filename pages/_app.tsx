5ffbebccee01369a24c84d8e2be638918e992e46
BobbyKJH <rlatnguscjsw@gmail.com>
2023-03-09 23:50:54 +0900

    Feat: React-query install
    1) 내용
    
    - React-query 설치
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
+/* React */
+import React, { useState } from 'react'
 import '../styles/globals.css'
+/* React-Query */
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools';
+/* Type */
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (    
+		<QueryClientProvider client={queryClient}>      
+			<Hydrate state={pageProps.dehydratedState}>       
+				<Component {...pageProps} />      
+			</Hydrate>
+      <ReactQueryDevtools initialIsOpen={true}/>
+		</QueryClientProvider>  
+	)
 }
 
-export default MyApp
+export default MyApp;
